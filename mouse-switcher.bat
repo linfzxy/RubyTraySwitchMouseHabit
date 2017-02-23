0fb9121d90fe4b36b884584c1da18bfec63f06d3
linfzxy <linfzxy@hotmail.com>
2017-02-23 18:23:10 +0800

    Use .bat file to run this app daemon

---
+++ mouse-switcher.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+:begin
+::下面是你自己的代码。
+ruby C:\Users\Lynn\IdeaProjects\MouseHandedQuickSwitch\mouse_switch.rb
--- mouse_switch.rb
+++ mouse_switch.rb
@@ -1,4 +1,4 @@
-class MouseSwitch
+﻿class MouseSwitch
   def self.switch_check is_right_handed
     @@lefthanded.always_show_image= is_right_handed==0
     @@righthanded.always_show_image= is_right_handed==1
@@ -9,7 +9,7 @@ class MouseSwitch
   Gtk::Settings.default.gtk_font_name="Microsoft YaHei Light 9"
   Gtk::UIManager.new
   si = Gtk::StatusIcon.new
-  trayicon=GdkPixbuf::Pixbuf.new :file => 'TrayIcon.png'
+  trayicon=GdkPixbuf::Pixbuf.new :file => File.dirname(__FILE__) + '/TrayIcon.png'
   si.set_pixbuf trayicon
 
   menu = Gtk::Menu.new
@@ -17,8 +17,8 @@ class MouseSwitch
   quit.label='退出'
   @@lefthanded=Gtk::ImageMenuItem.new :label => '左手鼠'
   @@righthanded=Gtk::ImageMenuItem.new :label => '右手鼠'
-  @@lefthanded.image=Gtk::Image.new(:file => 'check.png')
-  @@righthanded.image=Gtk::Image.new(:file => 'check.png')
+  @@lefthanded.image=Gtk::Image.new(:file => File.dirname(__FILE__) +'/check.png')
+  @@righthanded.image=Gtk::Image.new(:file => File.dirname(__FILE__) +'/check.png')
   is_right_handed=MouseStatusModify.check_mouse_state
   switch_check is_right_handed
 
